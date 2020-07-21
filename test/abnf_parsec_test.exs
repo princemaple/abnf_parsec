7e378685e9a49c5553f2c70805c686686f7eda14
Po Chen <chenpaul914@gmail.com>
2020-07-21 10:59:43 +1000

    Revert "Update abnf_parsec_test.exs"
    This reverts commit f40885482850be8b061f7d138616dcf646457b57.

--- test/abnf_parsec_test.exs
+++ test/abnf_parsec_test.exs
@@ -134,7 +134,7 @@ defmodule AbnfParsecTest do
     assert {:ok, [alpha_except_a_b_c: 'DEFXYZ'], "", %{}, {1, 0}, 6} =
              T.alpha_except_a_b_c("DEFXYZ")
 
-    assert {:error, "did not expect string \"A\" or ASCII character equal to 'B' or my_c", "A", %{}, {1, 0},
+    assert {:error, "did not expect string \"A\" or byte equal to ?B or my_c", "A", %{}, {1, 0},
             0} = T.alpha_except_a_b_c("A")
 
     assert {:error, _, "B", %{}, {1, 0}, 0} = T.alpha_except_a_b_c("B")
