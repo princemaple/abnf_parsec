0dda408e0c5c0ed245cb666718184360eee61293
Po Chen <chenpaul914@gmail.com>
2020-11-03 09:03:31 +1100

    handle 0 repeat

---
+++ test/abnf_parsec/edge_case_test.exs
@@ -0,0 +1,14 @@
+defmodule EdgeCaseTest do
+  use ExUnit.Case, async: true
+
+  test "0 repeat" do
+    defmodule ZeroRepetition do
+      use AbnfParsec,
+        abnf: """
+        path-empty = 0<blah>
+        """
+    end
+
+    assert {:ok, [path_empty: [""]], "", %{}, {1, 0}, 0} = ZeroRepetition.path_empty("")
+  end
+end
